7a16ba90c063c44ce2203dcbdbdfe3e90a2cfdd2
miguel <miguel@localhost>
2020-10-14 08:48:31 +0200

    Ejemplo de Aplicación Gráfica MVC - 1er Commit

---
+++ primeraAplicacionGrafica/src/main/java/com/mycompany/primeraaplicaciongrafica/controlador.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.primeraaplicaciongrafica;
+import com.mycompany.primeraaplicaciongrafica.modelo.*;
+import com.mycompany.primeraaplicaciongrafica.vista.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author matomas
+ */
+public class controlador {
+    
+    private static modelo modelo = new modelo();
+    private static vista vista = new vista();
+    
+    public static void main(String[] args) {
+        vista.setVisible(true);
+        Departamento dep = new Departamento();
+
+        vista.getBotonAceptarDepartamento().addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                
+                
+                if(vista.getOp()==Opcion.INSERCION){
+                  try {
+                    modelo.guardarDepartamento(vista.recuperarDatosDepartamento());
+                  } catch (SQLException ex) {
+                    vista.MostrarMensajeError("Inserción no realizada"+ex.getMessage());
+                  }  
+                }else if (vista.getOp()==Opcion.MODIFICACION){
+                    try {
+                        modelo.modificarDepartamento(vista.recuperarDatosDepartamento());
+                    } catch (SQLException ex) {
+                        vista.MostrarMensajeError("No se ha podido actualizar el departamento: "+ex.getMessage());
+                    }
+                }
+                
+            }
+        });
+        
+        vista.getBotonCargarScriptEjecutarScript().addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                File fichero = vista.selecionarFichero();
+                String ficheroString = leerFichero(fichero);
+                
+                modelo.ejecutarScript(fichero)
+                
+            }
+        });
+        
+        
+    }
+    
+    public void GestionarConexionBaseDatos(){
+        try {
+            modelo.ConectarBaseDatos();
+        } catch (SQLException ex) {
+           
+        }
+    }
+    
+    private String leerFichero(File fichero){
+        String ficheroleido=null;
+        
+        
+        
+        return ficheroleido;
+    }
+    
+    
+    
+}
---
+++ primeraAplicacionGrafica/src/main/java/com/mycompany/primeraaplicaciongrafica/modelo.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.primeraaplicaciongrafica;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author matomas
+ */
+public class modelo {
+    
+    private ResultSet resultado;
+    private Statement sentencia;
+    private Connection conexion;
+
+    public modelo(ResultSet resultado, Statement sentencia, Connection conexion) {
+        this.resultado = resultado;
+        this.sentencia = sentencia;
+        this.conexion = conexion;
+    }
+
+    public modelo() {
+    }
+
+    public ResultSet getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(ResultSet resultado) {
+        this.resultado = resultado;
+    }
+
+    public Statement getSentencia() {
+        return sentencia;
+    }
+
+    public void setSentencia(Statement sentencia) {
+        this.sentencia = sentencia;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+    
+    public Connection ConectarBaseDatos() throws SQLException{
+        
+        conexion=DriverManager.getConnection("jdbc:h2:./Database/database.db","miguel","12345");
+        return conexion;
+    }
+    
+    public int guardarDepartamento(Departamento dep) throws SQLException{
+        int lineasModificadas=0;
+        
+        String sql = "INSERT INTO departamentos VALUES (?,?,?)";
+        PreparedStatement sentencia = conexion.prepareStatement(sql);
+        
+        sentencia.setInt(1, dep.getId());
+        sentencia.setString(2, dep.getNombre());
+        sentencia.setString(3, dep.getLocalidad());
+        
+        lineasModificadas= sentencia.executeUpdate();
+        
+        return lineasModificadas;
+        
+        
+    }
+
+    public int modificarDepartamento(Departamento dep) throws SQLException {
+        int lineasModificadas=0;
+        
+        String sql = "UPDATE departamentos SET dnombre = ?, loc = ? Where dept_no = ?";
+        PreparedStatement sentencia = conexion.prepareStatement(sql);
+        
+        sentencia.setInt(3, dep.getId());
+        sentencia.setString(1, dep.getNombre());
+        sentencia.setString(2, dep.getLocalidad());
+        
+        lineasModificadas= sentencia.executeUpdate();
+        
+        return lineasModificadas;
+        
+    
+    }
+    
+    public int ejecutarScript (File fichero){
+        return 1;
+    }
+    
+    public int ejecutarScript (String rutaFichero){
+        return ejecutarScript(new File(rutaFichero));
+    }
+    
+    public int ejecutarUpdate (String script) throws SQLException{
+        Statement sentencia = conexion.createStatement();
+        return sentencia.executeUpdate(script);
+        
+    }
+    
+    
+    
+}
